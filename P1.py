de640bea4f13adee7aa411d2840b648431ced746
rdunavant0 <rdunavant0@gmail.com>
2019-01-22 15:46:08 -0600

    message

---
+++ P1.py
@@ -0,0 +1,32 @@
+def main():
+def problem1():
+def problem2():
+def problem3():
+def problem4():
+
+def problem1():
+    import random
+    number = random.randint(69)
+
+def problem2():
+    for .quit('q')
+    if not 'q':
+    string = input("Input another string")
+
+def problem3():
+    zero = input("Enter 0")
+    for zero
+    posInt = input("Enter positive integer")
+    print(0)
+    zero2 = input("Enter number again")
+    .repeat
+
+def problem4():
+    import random
+    number = random.randint(84)
+    userInput = input("Guess the random number")
+    for number
+    print(number)
+    .quit
+    if not number:
+    userinput2 = input("Tell user if guess has to be higher or lower")
